e9d2e2b7d9e4e4d4f97667faade836ee2cd146b1
Jason Callaway <jason@jasoncallaway.com>
2016-01-14 21:02:13 -0500

    Ready for testing, but Gluster configuration is not complete

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+printf "Refreshing EC2 cache..."
+#./inventory/aws/hosts/ec2.py --refresh-cache
+printf "done\n"
+
+ansible-playbook -vvv -i inventory/aws/hosts/ec2.py gluster_setup.yaml
