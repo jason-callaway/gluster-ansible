0a59bf1931831c82b428538ba6460df224d255e4
Jason Callaway <jason@jasoncallaway.com>
2016-01-14 23:36:39 -0500

    Closer still

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ printf "Refreshing EC2 cache..."
 #./inventory/aws/hosts/ec2.py --refresh-cache
 printf "done\n"
 
-ansible-playbook -i inventory/aws/hosts/ec2.py gluster_setup.yaml
+ANSIBLE_KEEP_REMOTE_FILES=1 ansible-playbook -vvvv -i inventory/aws/hosts/ec2.py gluster_setup.yaml
