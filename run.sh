bc1268c0f9598cd6e552d8eebac8a390ca0d2d5f
Jason Callaway <jason@jasoncallaway.com>
2016-01-16 13:35:14 -0500

    Fixed issue with sides a and b using the same az

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 printf "Refreshing EC2 cache..."
-#./inventory/aws/hosts/ec2.py --refresh-cache
+./inventory/aws/hosts/ec2.py --refresh-cache
 printf "done\n"
 
-ANSIBLE_KEEP_REMOTE_FILES=1 ansible-playbook -vvvv -i inventory/aws/hosts/ec2.py gluster_setup.yaml
+# -vvvvv
+# ANSIBLE_KEEP_REMOTE_FILES=1
+ansible-playbook -i inventory/aws/hosts/ec2.py gluster_setup.yaml
