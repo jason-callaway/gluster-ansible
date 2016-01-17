d999c482cc7171265b913ecb983cc5ac8dff85d3
Jason Callaway <jason@jasoncallaway.com>
2016-01-17 18:14:13 -0500

    Really close, just have to get Gluster peering to work

--- run.sh
+++ run.sh
@@ -4,8 +4,7 @@ printf "Refreshing EC2 cache..."
 ./inventory/aws/hosts/ec2.py --refresh-cache
 printf "done\n"
 
-# -vvvvv
-# ANSIBLE_KEEP_REMOTE_FILES=1
-ansible-playbook -vvv -i inventory/aws/hosts/ec2.py \
-                 --ssh-extra-args="-i ~/crypto/nsphexboard.pem" \
+#                 --ssh-extra-args="-i ~/crypto/nsphexboard.pem" \
+# For additional debug: -vvvvv and  ANSIBLE_KEEP_REMOTE_FILES=1
+ansible-playbook -v -i inventory/aws/hosts/ec2.py \
                  gluster_setup.yaml
