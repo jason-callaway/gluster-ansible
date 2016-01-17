271745f923f7cf275f97ffddf6c509ecea5ec3b9
Jason Callaway <jason@jasoncallaway.com>
2016-01-17 18:54:58 -0500

    Fixed a problem with subsequent runs wiping out the sg rules

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ printf "Refreshing EC2 cache..."
 ./inventory/aws/hosts/ec2.py --refresh-cache
 printf "done\n"
 
-#                 --ssh-extra-args="-i ~/crypto/nsphexboard.pem" \
 # For additional debug: -vvvvv and  ANSIBLE_KEEP_REMOTE_FILES=1
 ansible-playbook -v -i inventory/aws/hosts/ec2.py \
+                 --ssh-extra-args="-i ~/crypto/nsphexboard.pem" \
                  gluster_setup.yaml
