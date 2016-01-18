f6b83e61da0dfc9c2b01a3282aadd3f1c0bf7ad6
Jason Callaway <jason@jasoncallaway.com>
2016-01-18 10:25:48 -0500

    Cleanup

--- run.sh
+++ run.sh
@@ -4,7 +4,12 @@ printf "Refreshing EC2 cache..."
 ./inventory/aws/hosts/ec2.py --refresh-cache
 printf "done\n"
 
-# For additional debug: -vvvvv and  ANSIBLE_KEEP_REMOTE_FILES=1
-ansible-playbook -v -i inventory/aws/hosts/ec2.py \
-                 --ssh-extra-args="-i ~/crypto/nsphexboard.pem" \
-                 gluster_setup.yaml
+ssh-add -l >/dev/null 2>&1
+if [ "$?" != "0" ]
+then
+    # For additional debug: -vvvvv and ANSIBLE_KEEP_REMOTE_FILES=1
+    ansible-playbook -v -i inventory/aws/hosts/ec2.py gluster_setup.yaml
+else
+    echo "You must run this with ssh-agent holding a key via ssh-add"
+    exit 1
+fi
