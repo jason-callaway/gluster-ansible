23368233376afdd6edd970408d7d45d184b52496
Jason Callaway <jason@jasoncallaway.com>
2016-01-18 21:35:57 -0500

    I think I sorted out my first-vs-subsequent run problem by splitting the logic into two playbooks. This lets me refresh the dynamic ec2 inventory, which was the root of my problem.

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-printf "Refreshing EC2 cache..."
+printf "Refreshing EC2 cache (1 of 2)..."
 ./inventory/aws/hosts/ec2.py --refresh-cache
 printf "done\n"
 
@@ -8,6 +8,11 @@ ssh-add -l >/dev/null 2>&1
 if [ "${?}" == "0" ]
 then
     # For additional debug: -vvvvv and ANSIBLE_KEEP_REMOTE_FILES=1
+    ansible-playbook -v -i inventory/aws/hosts/ec2.py gluster_setup_prep.yaml
+    # We have to run two playbooks in order to update the dynamic ec2 inventory
+    printf "Refreshing EC2 cache (2 of 2)..."
+    ./inventory/aws/hosts/ec2.py --refresh-cache
+    printf "done\n"
     ansible-playbook -v -i inventory/aws/hosts/ec2.py gluster_setup.yaml
 else
     echo "You must run this with ssh-agent holding a key via ssh-add"
